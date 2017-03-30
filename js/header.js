970f33a71bdf2fda054281715d5184a619977826
xiaoma <670567137@qq.com>
2017-03-30 18:28:13 +0800

    daf

--- js/header.js
+++ js/header.js
@@ -50,87 +50,7 @@ $(".head-nav > ul li").each(function(i,element){
 
 
 
-  //未延时
-	// $('.head-nav > ul li').each(function(i,element){
- //            var _this = this;
-	// 	    $(this).hover(function(){
- //           		var $list = $('.list-wrap > div')[i];
- //           		$($list).css('display','block').siblings().css('display','none');
- //           		$(_this).find('a').addClass('nav-tabs-act');
- //           },function(){
- //           		$(_this).find('a').removeClass('nav-tabs-act');
- //           })
-	// })
-
-
-//  一个
-// $(".goods-list > ul li").each(function(i,element){
-//     // var that = $(this);
-//    		 var _this = this;
-// 			$(this).hoverDelay({
-//             outDuring: 0,
-//             hoverDuring: 100,
-// 			hoverEvent: function(){
-//                var $block = $('.goods-liston > div')[i];
-//  			$($block).css('display','block').siblings().css('display','none');
-//  			$(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff');
-//  			$($block).hover(function(){
-//  				$(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff');
-//  			},function(){
-//  				$(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
-//  			});
-
-//             },
-//             outEvent: function(){
-//               $(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
-//             }
-//         });
-    
-// });
-
-
-//两个
-
-	// goods-list 下的hover
-// 	$(".goods-list > ul li").each(function(i,element){
-//     // var that = $(this);
-//    		 var _this = this;
-// 			$(this).hoverDelay({
-//             hoverDuring: 100,
-//             outDuring: 100,
-// 			hoverEvent: function(){
-//               var $block = $('.goods-liston > div')[i];
-//  			$($block).css('display','block').siblings().css('display','none');
- 			
-// 					$($block).hoverDelay({
-// 					hoverDuring: 100,
-// 		            outDuring: 100,
-// 					hoverEvent: function(){
-// 		               $(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff');    
-
-// 		            },
-// 		            outEvent: function(){
-// 		               $(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
-// 		            }
-		        
-		    
-// 					});
-// 			$(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff');
-					
-//  			// $($block).hover(function(){
-//  			// 	$(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff');
-//  			// },function(){
-//  			// 	$(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
-//  			// });
-//             },
-//             outEvent: function(){
-//               $(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
-//             }
-//         });
-    
-// });
-
-		
+	  //nav-good list下的hover
 		$('.goods-list > ul li').each(function(i,element){
 		var _this = element; 
  		$(this).hover(function(){
@@ -147,11 +67,99 @@ $(".head-nav > ul li").each(function(i,element){
  		});	
 	});
   
-	
 
+
+   //goods 幻灯片的切换
+        $($('.carousel-img>img')[0]).addClass('img_active');
+        $($('.carousel-img span')[0]).addClass('span_active');
+        var now = 0;
+        function carousel(){
+            $('.carousel-bot span').each(function(i,ele){
+               
+                $(this).click(function(){
+                    now = $(this).index()-1;
+                    for(var j=0;j<4;j++){
+                    $($('.carousel-img>img')[j]).removeClass('img_active');
+                    $($('.carousel-bot span')[j]).removeClass('span_active');
+                        }
+                    $($('.carousel-bot span')[now]).addClass('span_active');
+                    $($('.carousel-img>img')[now]).addClass('img_active');
+                   });
+
+               });
+               
+                  $("#carousel-bot-p1").click(function(){
+
+                      if(now>0){
+                        now--;
+                        for(var j=0;j<4;j++){
+                             $($('.carousel-img>img')[j]).removeClass('img_active');
+                             $($('.carousel-bot span')[j]).removeClass('span_active');
+                            
+                             }
+                    $($('.carousel-bot span')[now]).addClass('span_active');
+                    $($('.carousel-img>img')[now]).addClass('img_active');
+                   
+                     }
+
+                 });
+                  $("#carousel-bot-p2").click(function(){
+                       
+                      autoplay();
+
+                 });
+              
+           
+
+
+            }
+             
+        carousel();
+        var timer = setInterval(function(){
+             autoplay();
+        },3000);
+
+         function autoplay(){
+            
+                        if(now<3){
+                        for(var j=0;j<4;j++){
+                             $($('.carousel-img>img')[j]).removeClass('img_active');
+                             $($('.carousel-bot span')[j]).removeClass('span_active');
+                         }
+                        now++;
+                        $($('.carousel-bot span')[now]).addClass('span_active');
+                        $($('.carousel-img>img')[now]).addClass('img_active');
+
+
+                         }else{
+                            now = -1;
+                         }
+                     } 
+        $('.carousel-bot').hover(function(){
+            clearInterval(timer);
+        },function(){
+            setInterval(function(){
+             autoplay();
+            },3000);
+        })
+       
+
+
+        // $('.carousel-text > ul li').each(function(i,element){
+        // var _this = element; 
+        // $(this).hover(
+        //     function()
+        //     {
+        //     $(_this).css('backgroundColor','#53a318').addClass('red green').find('a').css('color','#fff')}
+        //     ,function()
+        //     {
+        //         $(_this).css('backgroundColor','').removeClass('red green').find('a').css('color','#888');
+        //     }
+        //     );
+        // } 
 
 
 
 
   
-}) 
+});   //$domready  结束
